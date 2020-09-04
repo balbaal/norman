f25b7fb0c42bd3097ecc0e33a77d48b2084f9098
Balbaal <iqbal.mi20@gmail.com>
2020-09-04 16:23:46 +0800

    add: get -> request to show list media

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const apiRoot = require("./apiRoot");
+const { MEDIA_HOST } = process.env;
+const api = apiRoot(MEDIA_HOST);
 
-/* GET media listing. */
-router.get('/', function(req, res, next) {
-  res.send(`Media Service`);
+// get list media
+router.get("/", async (req, res) => {
+  try {
+    const mediaRes = await api.get("/media");
+
+    res.status(200).json({
+      status: "success",
+      message: "get all media list",
+      data: mediaRes.data,
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "error", message: "something wrong when get data" });
+  }
 });
 
 module.exports = router;
