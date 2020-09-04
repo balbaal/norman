ff2d9db64aa7dff2b9987d892b441f1099c1d798
Balbaal <iqbal.mi20@gmail.com>
2020-09-04 16:44:40 +0800

    add: handle delete media

--- routes/media.js
+++ routes/media.js
@@ -15,9 +15,41 @@ router.get("/", async (req, res) => {
       data: mediaRes.data,
     });
   } catch (error) {
+    if (error.code === "ECONNREFUSED") {
+      res
+        .status(500)
+        .json({ status: "error", message: "service api gateaway unavailable" });
+    }
+
     res
       .status(400)
       .json({ status: "error", message: "something wrong when get data" });
+  }
+});
+
+// delete media
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await api.delete(`media/${id}`);
+
+    res.status(201).json({
+      status: "success",
+      message: `image with id: ${id} has been deleted`,
+    });
+  } catch (error) {
+    if (error.code === "ECONNREFUSED") {
+      res
+        .status(500)
+        .json({ status: "error", message: "service api gateaway unavailable" });
+    }
+
+    console.log("error :>> ", error);
+
+    res
+      .status(error.response.status)
+      .json({ status: "error", message: "failed to delete image" });
   }
 });
 
