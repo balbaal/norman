114689ef618e7cb7589910705f352a0a0fba1898
Balbaal <iqbal.mi20@gmail.com>
2020-09-04 15:39:56 +0800

    fix: parse env timeout to int

--- routes/apiRoot.js
+++ routes/apiRoot.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
+const { TIMEOUT } = process.env;
 
 module.exports = (URL) => {
   return axios.create({
     baseURL: URL,
+    timeout: parseInt(TIMEOUT),
   });
 };
